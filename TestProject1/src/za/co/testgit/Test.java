3101212f3679e09948d0ce43a80713618bb04af4
Lofentse <Lofentse@gmail.com>
2022-08-05 00:09:52 +0200

    FirstCommit
    FirstCommit

---
+++ TestProject1/src/za/co/testgit/Test.java
@@ -0,0 +1,17 @@
+package za.co.testgit;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test t = new Test();
+		t.print();
+
+	}
+
+	private void print() {
+		// TODO Auto-generated method stub
+		System.out.println("My name is Lofentse");
+	}
+
+}
